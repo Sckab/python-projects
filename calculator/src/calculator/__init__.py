b99175bb74b28ce0b026f46b8f602f7066b14c6d
Giuliano <giulianodeamicis2009@gmail.com>
2025-09-27 15:37:38 +0200

    feat: updated the panel and added ask prompts

--- calculator/src/calculator/__init__.py
+++ calculator/src/calculator/__init__.py
@@ -1,12 +1,28 @@
 from rich import print as Print
 from rich.panel import Panel
+from rich.prompt import FloatPrompt
 
 
 def main() -> None:
     Print(
         Panel.fit(
-            "[bold green]Calculator[/bold green] is running!",
+            "[white]This is a simple calculator application.[/white]",
             title="CALCULATOR",
             subtitle="v1.0.0",
+            title_align="left",
+            subtitle_align="left",
+            style="green",
         )
+    )
+
+    firstNumber = FloatPrompt.ask(
+        "[green]Enter the first number[/green]",
+        default="0.0",
+        case_sensitive=False,
+    )
+
+    secondNumber = FloatPrompt.ask(
+        "[green]Enter the second number[/green]",
+        default="0.0",
+        case_sensitive=False,
     )
