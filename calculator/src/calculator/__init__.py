e00cd6a6942d3bae8a27b6ab4c720f4f92d73f44
Sckab <giulianodeamicis2009@gmail.com>
2025-09-27 20:36:10 +0200

    feat: completed the calculator

--- calculator/src/calculator/__init__.py
+++ calculator/src/calculator/__init__.py
@@ -1,6 +1,86 @@
 from rich import print as Print
 from rich.panel import Panel
-from rich.prompt import FloatPrompt
+from rich.prompt import FloatPrompt, Prompt
+from rich.rule import Rule
+
+
+def askNumbers():
+    global firstNumber, secondNumber
+
+    firstNumber = FloatPrompt.ask(
+        "[green]Enter the first number[/green]",
+        default="0.0",
+        case_sensitive=False,
+    )
+
+    secondNumber = FloatPrompt.ask(
+        "[green]Enter the second number[/green]",
+        default="0.0",
+        case_sensitive=False,
+    )
+
+
+def operations():
+    operation = Prompt.ask(
+        "[green]What operation do you want to do?[/green]",
+        choices=["+", "-", "*", "/"],
+        default="+",
+        case_sensitive=False,
+    )
+
+    while True:
+        match operation:
+            case "+":
+                Print(
+                    f"[bold green]Result:[/bold green] {firstNumber} + {secondNumber} = {firstNumber + secondNumber}"
+                )
+                break
+
+            case "-":
+                Print(
+                    f"[bold green]Result:[/bold green] {firstNumber} - {secondNumber} = {firstNumber - secondNumber}"
+                )
+                break
+
+            case "*":
+                Print(
+                    f"[bold green]Result:[/bold green] {firstNumber} * {secondNumber} {firstNumber * secondNumber}"
+                )
+                break
+
+            case "/":
+                if secondNumber == 0:
+                    print("You can't divide a number by 0")
+                    break
+
+                Print(
+                    f"[bold green]Result:[/bold green] {firstNumber} / {secondNumber} {firstNumber / secondNumber}"
+                )
+                break
+
+            case _:
+                print("Invalid input")
+
+
+def calculator():
+    while True:
+        Print(Rule(title="[bold green]INSERT THE NUMBERS[/bold green]"))
+
+        askNumbers()
+
+        Print(Rule(title="[bold green]SELECT THE OPERATION[/bold green]"))
+
+        operations()
+
+        want_continue = Prompt.ask(
+            "[green]Do you want to continue?[/green]",
+            choices=["yes", "no", "n", "y"],
+            default="n",
+            case_sensitive=False,
+        )
+
+        if want_continue == "n" or want_continue == "no":
+            break
 
 
 def main() -> None:
@@ -15,14 +95,4 @@ def main() -> None:
         )
     )
 
-    firstNumber = FloatPrompt.ask(
-        "[green]Enter the first number[/green]",
-        default="0.0",
-        case_sensitive=False,
-    )
-
-    secondNumber = FloatPrompt.ask(
-        "[green]Enter the second number[/green]",
-        default="0.0",
-        case_sensitive=False,
-    )
+    calculator()
