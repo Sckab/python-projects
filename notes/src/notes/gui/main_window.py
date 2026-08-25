278c4d6ca8da0aa05dcbee3f1e8a5225a7c0d85c
Sckab <giulianodeamicis2009@gmail.com>
2026-08-26 01:33:17 +0200

    switched to ui files

--- notes/src/notes/gui/main_window.py
+++ notes/src/notes/gui/main_window.py
@@ -1,16 +1,39 @@
 import os
 
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from notes.gui.ui.ui_main_window import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.counter = 0
+
+        self.ui.label.setText(str(self.counter))
+        self.ui.pushButton.clicked.connect(self.update_counter)
+
+    def update_counter(self, e):
+        self.counter += 1
+
+        self.ui.label.setText(str(self.counter))
 
 
 def run() -> int:
     script_dir = os.path.dirname(__file__)
 
-    app = QGuiApplication([])
+    styles_dir = os.path.join(script_dir, "styles")
 
-    engine = QQmlApplicationEngine()
-    engine.quit.connect(app.quit)
-    engine.load(os.path.join(script_dir, "qml/main.qml"))
+    app = QApplication([])
+
+    with open(os.path.join(styles_dir, "style.qss"), "r") as f:
+        app.setStyleSheet(f.read())
+
+    window = MainWindow()
+    window.show()
 
     return app.exec()
--- notes/src/notes/gui/qml/Style.qml
+++
@@ -1,7 +0,0 @@
-pragma Singleton
-import QtQuick 2.0
-
-QtObject {
-    property int textSize: 20
-    property color generalBackground: "#181616"
-}
